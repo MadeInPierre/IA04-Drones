6926a05b41acccbfad5016989e0f4922f4b8adb5
milesial <milesial@users.noreply.github.com>
2019-05-22 18:46:24 +0200

    Main file + nice gitignore

---
+++ src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import sim.display.Console;
+import sim.engine.SimState;
+
+public class Main {
+	public static void main(String[] args) {
+	}
+
+	}
+	public static void runWithoutUI(String[] args) {
+	}
+}
